ba01595dc503e0b0d403e9e534f6b3d01bd858c7
lena <lena@MacBook-Air-Alexandr.local>
2023-06-28 13:46:02 +0400

    add bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-v, --version', 'output the version number')
-  
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 program.parse();
