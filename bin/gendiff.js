2a1fefe7418f510cdbbbef50369360dba157e03e
lena <lena@MacBook-Air-Alexandr.local>
2023-06-28 13:22:18 +0400

    add bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
-import program from 'commander';
+
+const { program } = require('commander');
 
 program
-  .version('0.2.1')
+  .name('gendiff')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .option('-h, --help', 'output usage information');
+
+program.parse();
+  
+
