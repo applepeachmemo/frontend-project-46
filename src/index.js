c89100d1806cbbe8c6d51fb59be4c7fb57dd63fc
lena <lena@MacBook-Air-Alexandr.local>
2023-08-08 17:45:19 +0400

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { cwd } from 'node:process';
 import fs from 'fs';
-import { resolve, extname } from 'node:path';
+import { resolve } from 'node:path';
 import parsesFile from './parsers.js';
 import getTree from './getTree.js';
 import formatter from './formatters/index.js';
 
 const getFullFilePath = (filepath) => resolve(cwd(), filepath);
-
-const getFormat = (filepath) => extname(filepath).substring(1);
 
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
@@ -15,18 +13,15 @@ const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const pathFile1 = getFullFilePath(filepath1);
   const pathFile2 = getFullFilePath(filepath2);
 
-  const dataFile1 = readFile(pathFile1);
-  const dataFile2 = readFile(pathFile2);
+  const data1 = readFile(pathFile1);
+  const data2 = readFile(pathFile2);
 
-  const formatFile1 = getFormat(filepath1);
-  const formatFile2 = getFormat(filepath2);
-
-  const informationDiff = getTree(
-    parsesFile(dataFile1, formatFile1),
-    parsesFile(dataFile2, formatFile2),
+  const tree = getTree(
+    parsesFile(data1),
+    parsesFile(data2),
   );
 
-  return formatter(informationDiff, formatName);
+  return formatter(tree, formatName);
 };
 
 export default gendiff;
