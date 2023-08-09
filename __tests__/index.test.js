560f3dfdc05230f8e80de102bd20d5733f4d8e28
lena <lena@MacBook-Air-Alexandr.local>
2023-08-09 14:08:02 +0400

    add __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,29 +9,23 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedStylish = readFile('stylish.txt');
-const expectedPlain = readFile('plain.txt');
-const expectedJson = readFile('json.txt');
-
 describe('comparing files', () => {
-  const fileFormats = ['json', 'yaml'];
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
 
-  fileFormats.forEach((format) => {
-    describe(`format: ${format}`, () => {
-      const filepath1 = getFixturePath(`file1.${format}`);
-      const filepath2 = getFixturePath(`file2.${format}`);
+  const expectedStylish = readFile('stylish.txt');
+  const expectedPlain = readFile('plain.txt');
+  const expectedJson = readFile('json.txt');
 
-      test('plain format', () => {
-        expect(gendiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
-      });
+  test('default format (stylish)', () => {
+    expect(gendiff(filepath1, filepath2)).toEqual(expectedStylish);
+  });
 
-      test('json format', () => {
-        expect(gendiff(filepath1, filepath2, 'json')).toEqual(expectedJson);
-      });
+  test('plain format', () => {
+    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
+  });
 
-      test('default format', () => {
-        expect(gendiff(filepath1, filepath2)).toEqual(expectedStylish);
-      });
-    });
+  test('json format', () => {
+    expect(gendiff(filepath1, filepath2, 'json')).toEqual(expectedJson);
   });
 });
